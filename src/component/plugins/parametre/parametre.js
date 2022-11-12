e939f05c9950a012fb9d35c412f573e06137096e
Yakapi <realitygameserver@gmail.com>
2022-11-12 15:42:53 +0100

    init simpleaf freelance

---
+++ src/component/plugins/parametre/parametre.js
@@ -0,0 +1,16 @@
+
+export default function ParametrePlugin(){
+  return(
+    <div className="plugin_container">
+      <div className="plugin_brik"></div>
+      <div className="plugin_box">
+      <h2 className="title_plugin">Parametre</h2>
+      <div className="content_plugin">
+        <div className="content_box">
+        QSDQSDQSDQSDQSDQSDQSDQSDQSQSDQSDQSD
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/component/plugins/services/services.js
@@ -0,0 +1,11 @@
+export default function ServicesPlugin(){
+  return(
+    <div className="plugin_container">
+      <div className="plugin_brik"></div>
+      <div className="plugin_box">
+      <h2 className="title_plugin">Services</h2>
+      <div className="content_plugin">QSDQSDQSDQSDQSDQSDQSDQSDQSQSDQSDQSD</div>
+      </div>
+    </div>
+  )
+}
